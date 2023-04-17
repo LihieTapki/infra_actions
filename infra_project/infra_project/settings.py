696f5f317f6d5b3b109abf2bb6b4b7b51247e204
Илья Афанасьев <iafanasevmail@gmail.com>
2023-04-17 23:19:23 +0300

    test

--- infra_project/infra_project/settings.py
+++ infra_project/infra_project/settings.py
@@ -13,7 +13,9 @@ SECRET_KEY = 'k9gwcfq01xszx2(_4e#gc3u$rt=)y6ivx=$wt+5&-^ktk4dg4e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    51.250.13.176,
+]
 
 INSTALLED_APPS = [
     'infra_app.apps.InfraAppConfig',
