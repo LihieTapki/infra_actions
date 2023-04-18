2c34ead5b963e66135172780cdbfe1c9a0d8deb4
Илья Афанасьев <iafanasevmail@gmail.com>
2023-04-18 21:32:55 +0300

    add bot

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -21,4 +21,4 @@ class StaticPagesURLTests(TestCase):
         self.assertContains(response, 'Работает!')
 
         response = self.guest_client.get('/second/')
-        self.assertContains(response, 'А это вторая страница')
+        self.assertContains(response, 'А эта тоже работает')
